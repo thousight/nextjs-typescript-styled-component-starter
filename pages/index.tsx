9ee4318f75083e305b4a4ed448f66944f6a5e95e
Mark Wen <markwenguojie94@gmail.com>
2021-01-03 18:05:07 -0800

    Made this repo a template

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,26 @@
 import Head from 'next/head'
-import Link from 'next/link'
 import { FC } from 'react'
 import styled from 'styled-components'
 
 import packageJson from '../package.json'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: red;
+const Title = styled.p`
+  color: #333333;
 `
 
 const Home: FC = () => (
-  <div>
+  <>
     <Head>
       <title>{packageJson.name}</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <main>
-      <Title className="title">
-        <Link href="/posts">read this page</Link>
+      <Title>
+        {packageJson.name} at version {packageJson.version}
       </Title>
     </main>
-  </div>
+  </>
 )
 
 export default Home
